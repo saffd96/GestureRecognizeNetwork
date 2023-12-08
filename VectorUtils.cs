7268dafc3be83a652c2e2e7ddc8fbcf8be7496d4
saffd96 <58534446+saffd96@users.noreply.github.com>
2023-12-08 03:22:36 +0300

    add dll + utils

---
+++ VectorUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using NeuralNetwork;
+
+public static class VectorUtils
+{
+    public static List<SimpleVector> CheckForNaNValues(this List<SimpleVector> data)
+    {
+        var result = new List<SimpleVector>();
+
+        foreach (var v in data)
+        {
+            var newV = new SimpleVector(
+                float.IsNaN(v.X) ? 0 : v.X,
+                float.IsNaN(v.Y) ? 0 : v.Y,
+                float.IsNaN(v.Z) ? 0 : v.Z
+            );
+
+            result.Add(newV);
+        }
+
+        return result;
+    }
+}
